e23e72f197610d4ae1e683168fa14f2e725a572e
Johnny3527 <70180139+Johnny3527@users.noreply.github.com>
2020-10-15 12:59:07 -0500

    prueba de datos
    ...

--- web/index.php
+++ web/index.php
@@ -33,6 +33,18 @@ $app->post('/enviarDato', function (Request $request) use ($app) {
 
 //Ruta de demostración, se recibe(n) dato(s) y se manipulan
 $app->post('/newDato', function (Request $request) use ($app) {
+   
+    $dbconn = pg_pconnect("host=ec2-3-210-178-167.compute-1.amazonaws.com port=5432 dbname=des77jp9cat6qo user=bsntemegqjneun password=f4525a1d46d3754a0e203ca2c9f4f37b181bb300a0022301bbcf1604e71898ee")
+
+    if ($dbconn) {
+    	return "conectado"
+    	
+    }
+    else{
+    	return "no conectado"
+
+
+
    	$nombre = $request->get('nombre');
 	$respuesta = "Hola " .$nombre;
    	return $respuesta;
